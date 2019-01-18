c6383823f05c8e26c5806ff681017aae73f6ff82
Will Pease <william.pease@maine.edu>
2019-01-18 17:25:57 -0500

    #1 - Fix weirdness with JS compilation

--- src/jsMain/kotlin/sample/SampleJs.kt
+++ src/jsMain/kotlin/sample/SampleJs.kt
@@ -1,9 +1,18 @@
 package sample
 
+import react.dom.render
+import kotlin.browser.document
+
 actual class Sample {
     actual fun checkMe() = 12
 }
 
 actual object Platform {
     actual val name: String = "JS"
+}
+
+actual fun nativeMain(args: Array<String>) {
+    render(document.getElementById("react-root")) {
+        +"Hello World"
+    }
 }
